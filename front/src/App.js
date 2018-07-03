7851c260f6cd7b9fd455078c79a157c428d24f0b
Tann <tianen.chen@outlook.com>
2018-07-03 16:19:45 -0400

    finish pie/bar

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "antd";
 import NumberSelector from "./NumberSelector";
+import PieChart from "./Pie";
+import BarChar from "./Bar";
 
 const bkg = {
-    background: "grey",
+    background: "white",
     board: "solid 1px black",
     height: 400
 };
+
+const pieData = [
+    { item: "事例一", count: 40 },
+    { item: "事例二", count: 21 },
+    { item: "事例三", count: 17 },
+    { item: "事例四", count: 13 },
+    { item: "事例五", count: 9 }
+];
+
+const barData = [
+    { token: "natual", frequency: 23 },
+    { token: "language", frequency: 10 },
+    { token: "process", frequency: 5 },
+    { token: "hello", frequency: 20 },
+    { token: "world", frequency: 8 },
+    { token: "tomorrow", frequency: 12 },
+    { token: "come", frequency: 36 },
+    { token: "go", frequency: 9 }
+];
 
 class App extends Component {
     state = {
@@ -26,22 +47,21 @@ class App extends Component {
     };
 
     render() {
+        console.log(pieData);
         return (
             <div className="App">
                 <Row gutter={32}>
                     <Col span={8} style={bkg}>
                         <NumberSelector init={this.state} />
                     </Col>
-                    <Col span={16} style={bkg}>
-                        article scanning board
-                    </Col>
+                    <Col span={16} style={bkg} />
                 </Row>
                 <Row gutter={16}>
-                    <Col span={12} style={bkg}>
-                        pie chart
+                    <Col span={10} style={bkg}>
+                        <PieChart init={pieData} />
                     </Col>
-                    <Col span={12} style={bkg}>
-                        bar chart
+                    <Col span={14} style={bkg}>
+                        <BarChar init={barData} />
                     </Col>
                 </Row>
             </div>
